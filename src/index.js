b513703a5b9db7d1cfeb1a7184a96710c744c2cf
Kunal <kunalaryad@gmail.com>
2025-07-26 14:26:58 +0530

    Render Function Implemented

--- src/index.js
+++ src/index.js
@@ -26,20 +26,63 @@ function createTextElement(text) {
     }
 }
 
+// This function actually 
+// add the "element" node inside "container" node
+function render(element,container) {
+    // if the element type is TEXT_ELEMENT
+    // we create a text node instead of a regular node.
+    // element.type Examples => "div", "h1", "h2" etc etc
+    const dom = element.type === "TEXT_ELEMENT" 
+        ? document.createTextNode("")
+        : document.createElement(element.type)
+
+    
+    const isProperty = key => key !== "children"
+
+    // if the Props is not children
+    // then assign that element props to the node
+    // assign the element props to the node.
+    // Example => "style", "id", "class" etc etc
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach(name => {
+            dom[name] = element.props[name]
+        })
+
+    // Recursively adding all the Nodes to the Child
+    element.props.children.forEach(child => {
+        render(child,dom)
+    });
+
+    container.appendChild(dom)
+}
+
 // ZACT is the name of our library
 const Zact = {
-    createElement
+    createElement,
+    render
 }
 
 /** @jsx Zact.createElement */
-const element = Zact.createElement(
-    "div",
-    {id: "foo"},
-    Zact.createElement("a", null, "bar"),
-    Zact.createElement("b")
+const element = (
+    <div style="background: salmon">
+        <h1>Hello World</h1>
+        <h2 style="text-align:right">from Zact</h2>
+    </div>
 )
+
+// Babel will transform the above JSX into this below function
+// Zact.createElement(
+//    "div",
+//    {
+//     style: "background: salmon"
+//    },
+//    Zact.createElement("h1", null, "Hello World"),
+//    Zact.createElement("h2", {style: "text-align: right"}, "from Zact")
+// )
 
 const container = document.getElementById("root")
 
 
-ReactDOM.createRoot(container).render(element);
+// ReactDOM.createRoot(container).render(element);
+Zact.render(element,container)
